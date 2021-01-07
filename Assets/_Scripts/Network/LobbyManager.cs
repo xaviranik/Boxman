947f2a9384282f63ae4fbc162d9b41bdda482c1a
xavir <xaviranik.me@gmail.com>
2021-01-07 20:40:48 +0600

    add: duo matchmaking

--- Assets/_Scripts/Network/LobbyManager.cs
+++ Assets/_Scripts/Network/LobbyManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using System;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
@@ -44,11 +45,49 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     {
         MainMenuPanel.SetActive(false);
         QuickMatchPanel.SetActive(true);
+
+        PhotonNetwork.JoinRandomRoom();
     }
 
     public void StopMatchSearch()
     {
         MainMenuPanel.SetActive(true);
         QuickMatchPanel.SetActive(false);
+
+        if(PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        MakeRoom();
+    }
+
+    private void MakeRoom()
+    {
+        int roomID = UnityEngine.Random.Range(1000, 9999);
+        RoomOptions roomOptions = new RoomOptions() { IsVisible=true, IsOpen=true, MaxPlayers=6 };
+        PhotonNetwork.CreateRoom("R" + roomID, roomOptions);
+
+        Debug.Log("Room created! Waiting for other players.");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 2 && PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting game");
+            PhotonNetwork.LoadLevel("Lobby");
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+        Debug.Log("Left the Room");
     }
 }
